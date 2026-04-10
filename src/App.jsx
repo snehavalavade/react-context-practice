23174e561631fbe412efefa73853de436c83b9b8
Sneha <snehavalavade8@gmail.com>
2026-04-10 11:31:25 +0530

    passing context through intermediate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import Heading from "../src/components/Heading.jsx";
 import Section from "../src/components/Section.jsx";
 
-export default function Page() {
+export default function ProfilePage() {
     return (
         <Section>
-            <Heading>Title</Heading>
-            <Section>
-                <Heading>Heading</Heading>
-                <Heading>Heading</Heading>
-                <Heading>Heading</Heading>
-                <Section>
-                    <Heading>Sub-heading</Heading>
-                    <Heading>Sub-heading</Heading>
-                    <Heading>Sub-heading</Heading>
-                    <Section>
-                        <Heading>Sub-sub-heading</Heading>
-                        <Heading>Sub-sub-heading</Heading>
-                        <Heading>Sub-sub-heading</Heading>
-                    </Section>
-                </Section>
-            </Section>
+            <Heading>My Profile</Heading>
+            <post title="Hello traveller!" body="Read about my adventures" />
+            <AllPosts />
+        </Section>
+    );
+}
+
+function AllPosts() {
+    return (
+        <Section>
+            <Heading>Posts</Heading>
+            <RecentPosts />
+        </Section>
+    );
+}
+
+function RecentPosts() {
+    return (
+        <Section>
+            <Heading>Recent Posts</Heading>
+            <Post title="Flavours of lisbon" body="...those pasteis da nata!" />
+            <Post title="Cricket match" body="I loved it!" />
+        </Section>
+    );
+}
+
+function Post({ title, body }) {
+    return (
+        <Section>
+            <Heading>{title}</Heading>
+            <p>
+                <i>{body}</i>
+            </p>
         </Section>
     );
 }
